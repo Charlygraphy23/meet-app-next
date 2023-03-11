a8a5bce22021433483fa62d31bbfea0fa9b77fd0
charlygraphy23 <dipta.biswas231998@gmail.com>
2023-03-11 21:08:00 +0530

    updated

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,22 +1,20 @@
 import NextAuth, { Session, User } from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
 
-
 export default NextAuth({
-
-    providers: [
-        GoogleProvider({
-            clientId : process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret : process.env.GOOGLE_CLIENT_SECRET || "",
-            authorization: {
-                params: {
-                  prompt: "consent",
-                  access_type: "offline",
-                  response_type: "code"
-                }
-              }
-        })
-    ],
-    secret: process.env.NEXTAUTH_SECRET
-}) 
+	providers: [
+		GoogleProvider({
+			clientId: process.env.HD_GOOGLE_CLIENT_ID || "",
+			clientSecret: process.env.HD_GOOGLE_CLIENT_SECRET || "",
+			authorization: {
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code",
+				},
+			},
+		}),
+	],
+	secret: process.env.HD_NEXTAUTH_SECRET,
+});
