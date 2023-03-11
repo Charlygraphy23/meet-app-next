2c5ef4b7880f3db29e83537cec4ca98579448cda
charlygraphy23 <dipta.biswas231998@gmail.com>
2023-03-11 20:12:12 +0530

    updated

--- pages/[roomId]/index.tsx
+++ pages/[roomId]/index.tsx
@@ -23,27 +23,26 @@ const HomePage = () => {
 	const roomID = query.roomId;
 	const [stream, setStream] = useState<UserStream>({
 		video: false,
-		mute: true
+		mute: true,
 	} as UserStream);
 
 	const getVideo = useCallback(async (id: string) => {
 		try {
 			const _stream = await getLocalMediaStream({ video: true, audio: true });
 
-			setStream(prevState => ({
+			setStream((prevState) => ({
 				...prevState,
 				stream: _stream,
 				userId: id,
 				video: _stream.active,
 				mute: !_stream.getAudioTracks()[0].enabled,
 				loading: false,
-				loadingText: ""
+				loadingText: "",
 			}));
 		} catch (err) {
 			console.error(err);
 		}
 	}, []);
-
 
 	const handleSocket = useCallback(async () => {
 		const abortController = handleAbortController();
@@ -55,8 +54,7 @@ const HomePage = () => {
 			dispatch(getSocket(socket));
 		});
 
-		socket.on("disconnect", () => {
-		});
+		socket.on("disconnect", () => {});
 
 		return () => {
 			abortController.abort();
@@ -74,21 +72,25 @@ const HomePage = () => {
 		socket.emit("join-room", {
 			room: roomID,
 			userId: id,
-			name : session?.user?.name || ""
+			name: session?.user?.name || "",
 		});
 		setShowCallView(!showCallView);
 	}, [id, roomID, session?.user?.name, showCallView, socket]);
 
 	const updateStream = useCallback((stream: UserStream) => {
-		setStream(stream)
-	}, [])
+		setStream(stream);
+	}, []);
 
 	useEffect(() => {
 		if (ref.current) return;
 
 		// @ts-expect-error
 		ref.current = true;
-		setStream(prevState => ({ ...prevState, loading: true, loadingText: "Please wait..." }))
+		setStream((prevState) => ({
+			...prevState,
+			loading: true,
+			loadingText: "Please wait...",
+		}));
 		handleSocket();
 	}, [handleSocket]);
 
@@ -96,30 +98,35 @@ const HomePage = () => {
 		if (!id) return;
 
 		dispatch(addMyId(id));
-		setStream(prevState => ({ ...prevState, loading: true, loadingText: "Connecting..." }))
+		setStream((prevState) => ({
+			...prevState,
+			loading: true,
+			loadingText: "Connecting...",
+		}));
 		getVideo(id);
 	}, [id, dispatch, getVideo]);
 
 	useEffect(() => {
-
 		if (sessionRef.current === session) return;
 		// @ts-expect-error
-		sessionRef.current = session
+		sessionRef.current = session;
 
-
-		setStream(prevState => ({
+		setStream((prevState) => ({
 			...prevState,
-			name: session?.user?.name || ""
-		}))
-	}, [session])
-	console.log("DD" , showCallView , session)
+			name: session?.user?.name || "",
+		}));
+	}, [session]);
 
 	return (
 		<>
 			{showCallView && session ? (
 				<CallLayout peer={peer} stream={stream} update={updateStream} />
 			) : (
-				<HomeComponent switchView={handleViewSwitch} stream={stream} updateStream={updateStream} />
+				<HomeComponent
+					switchView={handleViewSwitch}
+					stream={stream}
+					updateStream={updateStream}
+				/>
 			)}
 		</>
 	);
